66082c8b2817b91b6119551d6e80582fd8377cbe
tealessa <92655162+tealessa@users.noreply.github.com>
2022-05-10 19:09:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "Первый урок ДжаваСкрипта";
+let screens = "Плоский, Угловой";
+let screenPrice = 25;
+let rollback = 15;
+let fullPrice = 25000;
+let adaptive = true;
+
+alert("Привет, дружище!");
+console.log("Мониторы", screens , "стоят", fullPrice, "рублей");
