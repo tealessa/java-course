4c0239d3f739cbc40d717bd86b33190b713ca471
tealessa <92655162+tealessa@users.noreply.github.com>
2022-05-10 21:20:14 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let title = "Первый урок ДжаваСкрипта";
-let screens = "Плоский, Угловой";
-let screenPrice = 25;
-let rollback = 15;
-let fullPrice = 25000;
-let adaptive = true;
-
-alert("Привет, дружище!");
-console.log("Мониторы", screens , "стоят", fullPrice, "рублей");
